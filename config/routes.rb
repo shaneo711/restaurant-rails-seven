357282d2fb78f3dc2205746bb2ca1f3b95caeefb
Shane Garland <shane.garland1@gmail.com>
2022-12-16 20:00:42 +0800

    Add restaurants controller and empty navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'public#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'restaurants#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
