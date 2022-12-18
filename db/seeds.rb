f2de039a59519be54dde973a2891144ed0b2a0e7
Shane Garland <shane.garland1@gmail.com>
2022-12-18 20:48:33 +0800

    Add fontawesome and rework login/signup forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+List.create(
+  {
+    name: "Gang Gang",
+    description: 'Restaurants for the calendar app.',
+    image_url: 'https://static.thehoneycombers.com/wp-content/uploads/sites/2/2022/03/sydney-restaurants-woodcut.png',
+    user_id: 1
+  }
+)
+5.times do
+  Restaurant.create(
+    {
+      name: Faker::Restaurant.name,
+      address: Faker::Address.full_address,
+      phone: Faker::PhoneNumber.cell_phone,
+      image_url: 'http://sushibarkakizaki.com/wp-content/uploads/2017/06/kaki_top1-2.jpg',
+      user_id: 1,
+      list_id: 1
+    }
+  )
+end
