fab48ef6aca40e6aa747d4168a198348dc976613
Shane Garland <shane.garland1@gmail.com>
2023-01-04 07:16:53 +1100

    Add private attribute to list

---
+++ db/migrate/20221231013604_add_private_to_lists.rb
@@ -0,0 +1,5 @@
+class AddPrivateToLists < ActiveRecord::Migration[7.0]
+  def change
+    add_column :lists, :private, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_26_194054) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_013604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_194054) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "private", default: false
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
